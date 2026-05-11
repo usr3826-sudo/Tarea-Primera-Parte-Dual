f506ff0f9eb5512755e3028d70795de8a5f69bb2
usr3826-sudo <usr3826@salesianos-lacuesta.net>
2026-05-11 09:24:43 +0200

    featClase Pedido, atributos y atributo estatico

---
+++ src/Net/Salesianos/Pedido/Pedido.java
@@ -0,0 +1,11 @@
+package Net.Salesianos.Pedido;
+
+public class Pedido {
+    
+    protected String codigo;
+    protected String cliente;
+    protected double importeBase;
+
+    protected static int totalPedidos = 0;
+
+}
