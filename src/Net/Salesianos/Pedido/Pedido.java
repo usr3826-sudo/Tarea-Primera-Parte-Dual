e1672a730826e74c6235657e18b33963685d8289
usr3826-sudo <usr3826@salesianos-lacuesta.net>
2026-05-11 09:30:59 +0200

    feat:Constructor vacio

--- src/Net/Salesianos/Pedido/Pedido.java
+++ src/Net/Salesianos/Pedido/Pedido.java
@@ -1,11 +1,16 @@
 package Net.Salesianos.Pedido;
 
 public class Pedido {
-    
+
     protected String codigo;
     protected String cliente;
     protected double importeBase;
 
     protected static int totalPedidos = 0;
+
+      public Pedido() {
+
+        totalPedidos++;
+    }
 
 }
