582877e034b7d1e5c458856ed112d2fea9e77dfc
usr3826-sudo <usr3826@salesianos-lacuesta.net>
2026-05-11 09:48:47 +0200

    feat:Getter y Setter

--- src/Net/Salesianos/Pedido/Pedido.java
+++ src/Net/Salesianos/Pedido/Pedido.java
@@ -23,4 +23,30 @@ public class Pedido {
     }
 
 
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public double getImporteBase() {
+        return importeBase;
+    }
+
+    public void setImporteBase(double importeBase) {
+        this.importeBase = importeBase;
+    }
+
+
+
 }
