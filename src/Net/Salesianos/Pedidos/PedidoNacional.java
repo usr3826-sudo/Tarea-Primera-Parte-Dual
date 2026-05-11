8024c8e06a5e61c8cc2562b7fcd00f54638e74e9
usr3826-sudo <usr3826@salesianos-lacuesta.net>
2026-05-11 10:28:03 +0200

    feat:Constructores

--- src/Net/Salesianos/Pedidos/PedidoNacional.java
+++ src/Net/Salesianos/Pedidos/PedidoNacional.java
@@ -4,5 +4,18 @@ public class PedidoNacional extends Pedido {
 
     private double peso;
     private boolean urgente;
-    
+
+    public PedidoNacional() {
+
+        super();
+    }
+
+    public PedidoNacional(String codigo, String cliente, double importeBase, double peso, boolean urgente) {
+
+        super(codigo, cliente, importeBase);
+
+        this.peso = peso;
+        this.urgente = urgente;
+    }
+
 }
