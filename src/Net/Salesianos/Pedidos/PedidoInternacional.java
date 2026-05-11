3e69704054f11177b53fcb7de0a7928c81a69de1
usr3826-sudo <usr3826@salesianos-lacuesta.net>
2026-05-11 16:34:08 +0200

    feat:Constructor

--- src/Net/Salesianos/Pedidos/PedidoInternacional.java
+++ src/Net/Salesianos/Pedidos/PedidoInternacional.java
@@ -4,5 +4,18 @@ public class PedidoInternacional extends Pedido {
 
     private String paisDestino;
     private boolean aduanas;
-    
+
+    public PedidoInternacional() {
+
+        super();
+    }
+
+    public PedidoInternacional(String codigo, String cliente, double importeBase, String paisDestino, boolean aduanas) {
+
+        super(codigo, cliente, importeBase);
+
+        this.paisDestino = paisDestino;
+        this.aduanas = aduanas;
+    }
+
 }
